c17dea4c5a62369ae0f50e03c9d5d1434d358cbe
choppa4745 <2pakh45@gmail.com>
2025-02-19 10:12:02 +0300

    a half of practice4

--- Practice2_tasks/task1.c
+++ Practice2_tasks/task1.c
@@ -2,7 +2,7 @@
 #include <stdlib.h> 
 
 typedef struct {
-    char name[50];
+    char name[20];
     int age;
     float gpa;
 } Student;
@@ -10,13 +10,13 @@ typedef struct {
 int main() {
     int n;
 
-    printf("Enter the number of students: ");
+    printf("Enter number of students: ");
     scanf("%d", &n);
 
     Student *students = (Student *)malloc(n * sizeof(Student));
    
     for (int i = 0; i < n; ++i) {
-        printf("Enter name, age and GPA for student %d: ", i + 1);
+        printf("Enter name, age and GPA for student : ");
         scanf("%s %d %f", students[i].name, &students[i].age, &students[i].gpa);
     }
 
@@ -24,7 +24,6 @@ int main() {
         printf("Name: %s\n", students[i].name);
         printf("Age: %d\n", students[i].age);
         printf("GPA: %.2f\n", students[i].gpa);
-        printf("\n");
     }
 
     return 0;
